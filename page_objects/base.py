90f3584ced642baabc7ce54bcfe935a636b59247
alexandr <yakhlakovalexandr>
2021-09-14 23:39:07 +0300

    Добавлены методы element_is_displayed, press_key

--- page_objects/base.py
+++ page_objects/base.py
@@ -1,12 +1,14 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class BasePage:
+    KEY = {'esc': Keys.ESCAPE}
+
     def __init__(self, driver: WebDriver):
         self.driver = driver
-        # self.driver.get(url)
 
     def get(self, url):
         self.driver.get(url)
@@ -18,10 +20,13 @@ class BasePage:
         self.driver.refresh()
 
     def get_text(self, selector, index=0):
-        element = self._element(selector, index)
+        element = self.__element(selector, index)
         return element.text
 
-    def _element(self, selector: dict, index: int):
+    def element_is_displayed(self, selector, index=0):
+        return self.__element(selector, index).is_displayed()
+
+    def __element(self, selector: dict, index: int):
         if 'id' in selector.keys():
             by = By.ID
             selector = selector['id']
@@ -34,16 +39,21 @@ class BasePage:
         return self.driver.find_elements(by=by, value=selector)[index]
 
     def _click(self, selector, index=0):
-        element = self._element(selector, index)
+        element = self.__element(selector, index)
         element.click()
 
     def _send_keys(self, selector, value, index=0):
-        element = self._element(selector, index)
+        element = self.__element(selector, index)
         element.clear()
         element.send_keys(value)
 
     def _move_to_element(self, selector, index=0):
-        element = self._element(selector, index)
+        element = self.__element(selector, index)
         actions = ActionChains(self.driver)
         actions.move_to_element(to_element=element)
         actions.perform()
+
+    def press_key(self, key):
+        if key in self.KEY.keys():
+            key = self.KEY[key]
+        ActionChains(self.driver).key_down(key).key_up(key).perform()
